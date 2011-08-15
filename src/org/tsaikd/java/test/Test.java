a1c0fbb783f58749bac01f6326492f7a55b4f1dc
tsaikd <cheng-feng.tsai@foxconn.com>
2011-08-15 18:16:50 +0800

    add solr client library

---
+++ src/org/tsaikd/java/test/Test.java
@@ -0,0 +1,16 @@
+package org.tsaikd.java.test;
+
+import org.apache.log4j.Logger;
+
+public class Test {
+	static Logger log = Logger.getLogger(Test.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		log.debug("Start");
+		log.debug("End");
+	}
+
+}
